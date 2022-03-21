488cc9149c620e6eefc652743f000a51a325b29d
iyanuloluwa <iremide.nexzy@gmail.com>
2022-03-21 13:44:44 +0100

    django commit

---
+++ Calc/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return render(request,'home.html',{'name':'Django_developer'})
+
+def add(request):
+    value1 = int(request.POST["num1"])
+    value2 = int(request.POST["num2"])
+    result = value1 + value2
+    return render(request,'result.html',{'result':result})    
